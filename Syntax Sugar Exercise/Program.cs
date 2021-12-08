f9e1bb09178a8c9e8bcbcf20a4c3e1c5e1ce9a60
tray190 <belltracy411@gmail.com>
2021-12-08 00:52:51 -0600

    Syntax Sugar

---
+++ Syntax Sugar Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Syntax_Sugar_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             string personName = "Tracy";
+             string petName = "Binky";
+             string snackName = "life savers";
+             Console.WriteLine($"My name is {personName} I have a dog name {petName} and I like {snackName} snacks");
+
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+                Console.WriteLine(response);
+            }
+
+        }
+    }
+}
